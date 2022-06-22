77f75e204a68d93d939f18cd856f7ce7ff80839a
srikanth <94035252+srikanthA4@users.noreply.github.com>
2022-06-22 12:26:17 +0530

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <>
     <Helmet>
-      <title>404 Not Found |  E-Commerce</title>
+      <title>404 Not Found |  Digitalbay</title>
       <meta name='description' content='404 Not Found' />
     </Helmet>
     <Title order={3} style={{ marginBottom: '10px' }}>404 Page Not Found!</Title>
