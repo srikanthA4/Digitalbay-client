a287b62caa0286779b41688d00dfc2bed50d5e4f
srikanth <94035252+srikanthA4@users.noreply.github.com>
2022-06-26 01:16:05 +0530

    Update EditUser.jsx

--- src/components/Admin/EditUser.jsx
+++ src/components/Admin/EditUser.jsx
@@ -45,7 +45,7 @@ const EditProduct = ({ editId, setEdit }) => {
   return (
     <>
     <Helmet>
-      <title>Edit {username} | Apple E-Commerce</title>
+      <title>Edit {username} | Digitalbay</title>
       <meta name='description' content='Edit Account' />
     </Helmet>
     {
